549ac536274d22fbc0eb8dc8e5f2515d51a1f556
Bobby Byrnes <rbyrnes@square-9.com>
2020-04-02 14:17:24 -0400

    Initial commit for Delete Page Range Node
    Add Delete Page Range Node project and solution to the repository.

---
+++ DeletePageRange/DeletePageRange/PageDeletion.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Square9.CustomNode;
+
+namespace DeletePageRange
+{
+    public class PageDeletion : CustomNode
+    {
+        public override void Run()
+        {
+            if (Process.ProcessType != ProcessType.GlobalCapture)
+            {
+                LogHistory("\"Delete Page Range\" may only be used for GlobalCapture processes.");
+                return;
+            }
+
+            var pagesToDelete = ReadPagesSetting();
+            if (pagesToDelete.Count < 1)
+            {
+                LogHistory("No pages were set for deletion.");
+                return;
+            }
+
+            var documentPageCount = Process.Document.GetPages().Count;
+
+            pagesToDelete.Reverse();
+            foreach (var page in pagesToDelete)
+            {
+                if (page > documentPageCount)
+                {
+                    LogHistory($"Page to delete ({page}) is outside of the document page range ({documentPageCount}). Page deletion stopped.");
+                    return;
+                }
+                Process.Document.RemovePage(page - 1);
+                documentPageCount--;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the provided list of pages to be deleted.
+        /// </summary>
+        /// <returns></returns>
+        private List<int> ReadPagesSetting()
+        {
+            try
+            {
+                var pages = new List<int>();
+
+                var pagesSetting = Settings.GetStringSetting("Pages");
+                var pageGroups = pagesSetting.Split(',');
+
+                foreach (var pageGroup in pageGroups)
+                {
+                    var pageRange = pageGroup.Split('-');
+
+                    if (pageRange.Length == 2)  // page range
+                    {
+                        if (int.TryParse(pageRange[0], out var startPage) && int.TryParse(pageRange[1], out var endPage))
+                        {
+                            var rangeCount = endPage - startPage + 1; // The page range is inclusive, so add 1.
+                            pages.AddRange(Enumerable.Range(startPage, rangeCount));
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Page range must contain valid integers.");
+                        }
+                    }
+                    else if (pageRange.Length == 1)  // single page
+                    {
+                        if (int.TryParse(pageRange[0], out var page))
+                        {
+                            pages.Add(page);
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Page selection must be a valid integer.");
+                        }
+                    }
+                    else
+                    {
+                        throw new FormatException("Page range was entered in an invalid format.");
+                    }
+                }
+
+                pages.Sort();
+                return pages;
+            }
+            catch (Exception ex)
+            {
+                LogHistory("Unable to read Pages setting: " + ex.Message);
+                return new List<int>();
+            }
+        }
+    }
+}

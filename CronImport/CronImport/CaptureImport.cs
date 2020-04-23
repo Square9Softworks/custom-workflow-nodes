8698a5506ac4846de346f9833ade2a536aaaf6af
Bobby Byrnes <rbyrnes@square-9.com>
2020-04-23 16:59:01 -0400

    Initial commit for CRON Import node
    This version of the assembly just accepts a CRON expression string; it currently does not include a way to build the string in the UI.

---
+++ CronImport/CronImport/CaptureImport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Cronos;
+using Square9.CustomNode;
+
+namespace CronImport
+{
+    public class CaptureImport : CaptureImporter
+    {
+        public override List<string> Import()
+        {
+            var savedExpression = Settings.GetStringSetting("CronExpression");
+            var cronExpression = CronExpression.Parse(savedExpression);
+
+            var currentTime = DateTime.UtcNow;
+            var roundedTime = currentTime.Date + new TimeSpan(currentTime.Hour, currentTime.Minute, 0); // Disregard seconds in the current time comparison.
+
+            DateTime? nextTime = cronExpression.GetNextOccurrence(roundedTime, true);
+
+            if (!nextTime.HasValue)
+            {
+                throw new FormatException("Provided CRON Expression is not valid.");
+            }
+
+            if (roundedTime == nextTime)
+            {
+                var importDirectory = Settings.GetStringSetting("SourcePath");
+                return Directory.GetFiles(importDirectory).ToList();
+            }
+
+            return new List<string>();
+        }
+    }
+}

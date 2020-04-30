5b5c73b299c0bcd6072428b58e5cc7809aec6c72
Bobby Byrnes <rbyrnes@square-9.com>
2020-04-30 17:08:28 -0400

    Update CRON Import to use the last recorded run time for comparison.
    This allows engine start-ups to pickup scheduled imports that were missed if the engine was off.

--- CronImport/CronImport/CaptureImport.cs
+++ CronImport/CronImport/CaptureImport.cs
@@ -10,28 +10,64 @@ namespace CronImport
 {
     public class CaptureImport : CaptureImporter
     {
+        string LastRunFile = "";
+
         public override List<string> Import()
         {
+            LastRunFile = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + $@"\Square_9_Softworks\CronImport\{Workflow.ID}\LastExecution.txt";
+
             var savedExpression = Settings.GetStringSetting("CronExpression");
             var cronExpression = CronExpression.Parse(savedExpression);
 
-            var currentTime = DateTime.UtcNow;
-            var roundedTime = currentTime.Date + new TimeSpan(currentTime.Hour, currentTime.Minute, 0); // Disregard seconds in the current time comparison.
-
-            DateTime? nextTime = cronExpression.GetNextOccurrence(roundedTime, true);
+            var lastRunTime = GetLastRunTime();
+            DateTime? nextTime = cronExpression.GetNextOccurrence(lastRunTime);
 
             if (!nextTime.HasValue)
             {
                 throw new FormatException("Provided CRON Expression is not valid.");
             }
 
-            if (roundedTime == nextTime)
+            if (DateTime.UtcNow.Ticks >= nextTime.Value.Ticks)
             {
                 var importDirectory = Settings.GetStringSetting("SourcePath");
-                return Directory.GetFiles(importDirectory).ToList();
+                var filesToImport = Directory.GetFiles(importDirectory).ToList();
+
+                RecordLastRunTime(DateTime.UtcNow);
+
+                return filesToImport;
             }
 
             return new List<string>();
+        }
+
+        /// <summary>
+        /// Retrieves the last recorded time the workflow import was run.
+        /// </summary>
+        /// <returns></returns>
+        private DateTime GetLastRunTime()
+        {
+            if (File.Exists(LastRunFile))
+            {
+                var lastRunDateTime = File.ReadAllText(LastRunFile);
+                return DateTime.Parse(lastRunDateTime).ToUniversalTime();
+            }
+            else
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(LastRunFile));
+
+                var newDateTime = new DateTime(0, DateTimeKind.Utc);
+                File.WriteAllText(LastRunFile, newDateTime.ToString());
+                return newDateTime;
+            }
+        }
+
+        /// <summary>
+        /// Updates the workflow import's last run time.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        private void RecordLastRunTime(DateTime dateTime)
+        {
+            File.WriteAllText(LastRunFile, dateTime.ToString());
         }
     }
 }
